072e939240dcd2f8234fb665a529ac2522df5db6
mnburmaa <58815277+mnburmaa@users.noreply.github.com>
2020-03-15 21:26:26 -0500

    Update MapperClass.java

--- AverageComputation/src/part4/MapperClass.java
+++ AverageComputation/src/part4/MapperClass.java
@@ -10,8 +10,6 @@ import part2.PairWritable;
 
 public class MapperClass extends Mapper<Object, Text, PairWritable, IntWritable> {
 
-//	PairW pair = new PairW();
-//	IntWritable one = new IntWritable(1);
 	@Override
 	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 		String[] record = value.toString().split(" ");
@@ -22,24 +20,5 @@ public class MapperClass extends Mapper<Object, Text, PairWritable, IntWritable>
 				context.write(new PairWritable(record[i], record[j]), new IntWritable(1));
 			}
 		}
-		// for(int i=0; i<record.length; i++) {
-//	    	String s = record[i].trim();
-//
-//	    	if(!s.matches("[A-Z]{1}\\d+"))
-//	    		continue;
-//
-//	    	for(int j=i+1; j<record.length; j++) {
-//	    		String s2 = record[j].trim();
-//
-//	    		if(!s2.matches("[A-Z]{1}\\d+"))
-//		    		continue;
-//
-//	    		else if(s.equals(s2)) break;
-//
-//	    		pair.setKey(s);
-//	    		pair.setValue(s2);
-//				context.write(pair, one);
-//	    	}
-//	    }
 	}
 }
